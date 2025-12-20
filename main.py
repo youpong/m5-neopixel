d7ca6dd6a4fe5f23b992b6cc53768dfe887b0529
NAKAJIMA Yusaku <yupong7@gmail.com>
2025-12-21 01:04:10 +0900

    Add space before comment

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def setup():
     Widgets.setRotation(0)
     Widgets.fillScreen(0x000000)
 
-    rgb26 = RGB(io=26, n=PIXEL_NUM, type="WS2812") # Neopixel
+    rgb26 = RGB(io=26, n=PIXEL_NUM, type="WS2812")  # Neopixel
 
 
 def shift_color(c, arr):
